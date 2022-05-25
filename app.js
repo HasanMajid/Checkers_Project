c721e48a298f1087d812fc89418b1a72ad4c25b4
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-05-25 15:07:53 -0400

    Changed most of the code and got the top pieces moving down properly Co-authored-by: AlejandroMedina <Nova2077@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,131 +1,18 @@
-let rows = [];
-let pieces = [];
+var rows = [];
+var pieces = [];
+var squares = [];
+var grid = document.querySelector("#grid");
 
-var activePiece;
-var activeI;
-var activeJ;
 
-let active = function (e, index, i, j) {
-    pieces[index] = e;
-    return function () {
-        if (e === activePiece) {
-            e.classList.remove("active");
+// Piece.prototype.moveDownRight = function () {
+//     let {piece,index} = this;
+//     piece.style = "background-color:black;";
+//     // piece.parentElement;
 
-            activePiece = null;
-            if (rows[activeI + 1].childNodes[activeJ - 1]) {
-                rows[activeI + 1].childNodes[activeJ - 1].style = "background-color:black;";
-            }
-            if (rows[activeI + 1].childNodes[activeJ + 1]) {
-                rows[activeI + 1].childNodes[activeJ + 1].style = "background-color:black;";
-            }
-
-            return
-        }
-
-        if (activeI || activeJ) {
-            if (rows[activeI + 1].childNodes[activeJ - 1]) {
-                rows[activeI + 1].childNodes[activeJ - 1].style = "background-color:black;";
-            }
-            if (rows[activeI + 1].childNodes[activeJ + 1]) {
-                rows[activeI + 1].childNodes[activeJ + 1].style = "background-color:black;";
-            }
-        }
-        e.classList.remove("active");
-        if (e === activePiece) {
-            activePiece.classList.remove("active");
-            activePiece = null;
-            console.log("true")
-            return
-        }
-
-        activeI = i;
-        activeJ = j;
-        if (!pieces[index + 7] || !pieces[index + 9]) {
-            if (rows[activeI + 1].childNodes[activeJ - 1] && !pieces[index + 7]) {
-                rows[activeI + 1].childNodes[activeJ - 1].style = "background-color:cyan;";
-                rows[activeI + 1].childNodes[activeJ - 1].onclick = moveDownLeft(e, index);
-            }
-            if (rows[activeI + 1].childNodes[activeJ + 1] && !pieces[index + 9]) {
-                rows[activeI + 1].childNodes[activeJ + 1].style = "background-color:cyan;";
-                rows[activeI + 1].childNodes[activeJ + 1].onclick = moveDownRight(e, index);
-            }
-            if (activePiece) {
-                activePiece.classList.remove("active");
-            }
-            e.classList.add("active");
-            activePiece = e;
-        }
-    }
-}
-
-// function active(e){
-//     active.classList.remove("active");
-//     return function () {
-//         e.classList.add("active"); 
-//     }
 // }
 
-let moveDownRight = function (e, index) {
-    var moveby = 0;
-    pieces[index] = e;
-    return function () {
-        if (!pieces[index + 9]) {
-            moveby += 72.5;
-            pieces[index] = false;
-            index += 9;
-            pieces[index] = e;
-            e.style = `transform: translate(${moveby}px, ${moveby}px)`;
 
-        }
-    };
-}
-
-let moveDownLeft = function (e, index) {
-    var moveby = 0;
-    pieces[index] = e;
-    return function () {
-        if (!pieces[index + 7]) {
-            moveby += 72.5;
-            pieces[index] = false;
-            index += 9;
-            pieces[index] = e;
-            e.style = `transform: translate(${moveby * (-1)}px, ${moveby}px)`;
-        }
-    };
-}
-
-let moveUpRight = function (e, index) {
-    var moveby = 0;
-    pieces[index] = e;
-    // this.index = index;
-    return function () {
-        if (!pieces[index - 9]) {
-            moveby -= 72.5;
-            e.style = `transform: translate(${moveby * (-1)}px, ${moveby}px)`;
-            pieces[index] = false;
-            index -= 9;
-            pieces[index] = e;
-            console.log(pieces);
-        }
-    };
-}
-
-let moveUpLeft = function (e, index) {
-    var moveby = 0;
-    pieces[index] = e;
-    // this.index = index;
-    return function () {
-        if (!pieces[index - 9]) {
-            moveby -= 72.5;
-            e.style = `transform: translate(${moveby}px, ${moveby}px)`;
-            pieces[index] = false;
-            index -= 9;
-            pieces[index] = e;
-            console.log(pieces);
-        }
-    };
-}
-
+//////////////// Squares /////////////////////////////////////////
 for (let i = 0; i < 8; i++) {
     rows[i] = document.createElement("div");
     rows[i].className = "row";
@@ -133,45 +20,153 @@ for (let i = 0; i < 8; i++) {
         var square = document.createElement("div");
         square.className = "square";
 
-
         rows[i].appendChild(square);
+        if (j % 2 !== i % 2) {
+            squares.push(square);
+        }
     }
-
-    grid = document.querySelector("#grid")
     grid.appendChild(rows[i])
-
 }
 
-///////////////////// Top Pieces ////////////////////////////////////
+function Piece(index, piece, row) {
+    this.index = index;
+    this.piece = piece;
+    this.className = "pieceUp";
+    this.row = row;
+    let adder = 4;
+
+    piece.onclick = moveDown;
+
+    function moveDown() {
+        if (row % 2 == 1) {
+            adder = 3;
+        }
+        else {
+            adder = 4;
+        }
+        squares[index + adder].style = "background-color:cyan;";
+
+        squares[index + adder].onclick = function () {
+            squares[index + adder].style = "background-color:black;";
+            squares[index + adder + 1].style = "background-color:black;";
+
+            squares[index + adder + 1].onclick = null;
+            squares[index + adder ].onclick = null;
+            let moveby = 72.5;
+            piece.style = `transform: translate(${moveby * (-1)}px, ${moveby}px)`;
+            // let clonedNode = piece.cloneNode(true);
+            let animate = function () {
+                squares[index].firstChild.remove();
+                index += adder;
+                row += 1;
+                squares[index].appendChild(piece);
+                piece.style = `transform: translate(0px,0px)`;
+            }
+            myTimeout = setTimeout(animate, 500);
+            console.log(squares);
+        }
+        squares[index + adder + 1].style = "background-color:cyan;";
+        
+        squares[index + adder + 1].onclick = function () {
+            squares[index + adder + 1].style = "background-color:black;";
+            squares[index + adder].style = "background-color:black;";
+
+            squares[index + adder + 1].onclick = null;
+            squares[index + adder ].onclick = null;
+            let moveby = 72.5;
+            piece.style = `transform: translate(${moveby}px, ${moveby}px)`;
+            // let clonedNode = piece.cloneNode(true);
+            let animate = function () {
+                squares[index].firstChild.remove();
+                index += adder + 1;
+                row += 1;
+                squares[index].appendChild(piece);
+                piece.style = `transform: translate(0px,0px)`;
+            }
+            myTimeout = setTimeout(animate, 500);
+
+            console.log(squares);
+        }
+
+    }
+}
+
+// Piece.prototype.downLeft = function () {
+//     let { index, piece, row } = this;
+//     console.log(index);
+//     let moveby = 72.5;
+//     piece.style = `transform: translate(${moveby * (-1)}px, ${moveby}px)`;
+//     // let clonedNode = piece.cloneNode(true);
+//     let animate = function () {
+//         squares[index].firstChild.remove();
+//         if (row % 2 == 1) {
+//             index += 3;
+//         }
+//         else {
+//             index += 4;
+//         }
+//         row += 1;
+//         squares[index].appendChild(piece);
+//         piece.style = `transform: translate(0px,0px)`;
+//     }
+//     myTimeout = setTimeout(animate, 500);
+
+//     console.log(squares);
+
+// }
+
+/////////////////// Top Pieces ////////////////////////////////////
+let indexTop = 0;
 for (let i = 0; i < 3; i++) {
     for (let j = 1; j < 8; j += 2) {
         if (i == 1) { j--; }
-        let piece = document.createElement("div");
+        var piece = document.createElement("div");
         piece.className = "pieceUp";
         rows[i].childNodes[j].appendChild(piece);
 
-        // piece.onclick = moveDown(piece, (i * 8) + j);
-        // piece.onclick = moveDownRight(piece,(i * 8) + j);
-        piece.onclick = active(piece, (i * 8) + j, i, j);
+
+        const p = new Piece(indexTop, piece, i);
+
+        indexTop += 1;
+        console.log(p.index)
+
+        // piece.onclick = p.downLeft;
+        // piece.onclick = p.moveDownRight;
+        // moveDownRight(piece)
         if (i == 1) { j++; }
     }
 }
 
-///////////////////////////// Bottom Pieces /////////////////////////////////////////////////////
+// for (let i = 0; i < 11; i++) {
+//     pieces[i] = new Piece(i);
+//     let piece = document.createElement("div");
+//     piece.className = "pieceUp";
+//     // rows[i].childNodes[j].appendChild(piece);
 
+// }
+
+///////////////////////////// Bottom Pieces ///////////////////////////
+let indexBot = 20;
 for (let i = 5; i < 8; i++) {
     for (let j = 0; j < 8; j += 2) {
         if (i == 6) { j++; }
-        let piece = document.createElement("div");
+        var piece = document.createElement("div");
         piece.className = "pieceDown";
         rows[i].childNodes[j].appendChild(piece);
 
-        piece.onclick = moveUpLeft(piece, (i * 8) + j, i, j);
+        const p = new Piece(indexBot, piece, i);
+        indexBot += 1;
+        console.log(p.index)
+
         if (i == 6) { j--; }
     }
 }
 
-console.log(pieces);
+// for (let i = 20; i < 31; i++) {
+//     pieces[i] = new Piece(i);
+// }
+
+console.log(squares);
 
 
 
